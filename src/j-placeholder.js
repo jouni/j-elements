2d80195bd2ea98f91caf83f6e67a6f67b588a849
Jouni Koivuviita <jouni@vaadin.com>
2018-07-04 22:45:40 +0300

    Add j-placeholder
    A helper element to display a placeholder.

---
+++ src/j-placeholder.js
@@ -0,0 +1,60 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      width: 100px;
+      height: 100px;
+      overflow: hidden;
+      color: rgba(0, 0, 0, 0.3);
+      font-size: 0.875em;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    svg {
+      display: block;
+      stroke-width: 1px;
+      stroke: currentColor;
+      width: 100%;
+      height: 100%;
+      opacity: 0.5;
+      border: 1px solid currentColor;
+      box-sizing: border-box;
+    }
+
+    .j-placeholder-content {
+      transform: translateY(-100%);
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+  <svg viewBox="0 0 100 100" preserveAspectRatio="none">
+    <path d="M0,100 L100,0" vector-effect="non-scaling-stroke"></path>
+    <path d="M100,100 L0,0" vector-effect="non-scaling-stroke"></path>
+  </svg>
+  <div class="j-placeholder-content">
+    <slot></slot>
+  </div>
+`;
+
+export class JPlaceholder extends HTMLElement {
+
+  connectedCallback() {
+    if (!this.shadowRoot) {
+      if (typeof ShadyCSS != 'undefined' && !ShadyCSS.nativeShadow) {
+        ShadyCSS.prepareTemplate(template, this.nodeName.toLowerCase());
+        ShadyCSS.styleElement(this);
+      }
+      this.attachShadow({mode: 'open'});
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+  }
+
+}
+
+window.customElements.define('j-placeholder', JPlaceholder);
