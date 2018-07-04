1d8e6e833d020675ddc17a8c46b8f9741251cb72
Jouni Koivuviita <jouni@vaadin.com>
2018-07-04 22:47:32 +0300

    Update package main file to export all components

---
+++ elements.js
@@ -0,0 +1,23 @@
+import {JAppLayout} from './src/j-app-layout.js';
+import {JAvatar} from './src/j-avatar.js';
+import {JCard} from './src/j-card.js';
+import {JDialog} from './src/j-dialog.js';
+import {JField} from './src/j-field.js';
+import {JIcon} from './src/j-icon.js';
+import {JPlaceholder} from './src/j-placeholder.js';
+import {JTooltip} from './src/j-tooltip.js';
+import {StylableMixin} from './src/stylable-mixin.js';
+import {TeleportingElement} from './src/teleporting-element.js';
+
+export {
+  JAppLayout,
+  JAvatar,
+  JCard,
+  JDialog,
+  JField,
+  JIcon,
+  JPlaceholder,
+  JTooltip,
+  StylableMixin,
+  TeleportingElement
+};
