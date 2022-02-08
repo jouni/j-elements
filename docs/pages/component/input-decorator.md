745634c2fdea537cad750ea9a625758d828f87ed
Jouni Koivuviita <jouni@vaadin.com>
2022-02-08 20:34:21 +0200

    Initial InputDecorator implementation

--- docs/pages/component/input-decorator.md
+++ docs/pages/component/input-decorator.md
@@ -11,3 +11,23 @@ eleventyNavigation:
 ```javascript
 import { InputDecorator } from 'j-elements/src/components/InputDecorator.js';
 ```
+
+<style>
+input {
+  padding: 0.5em;
+  height: 2rem;
+}
+
+[slot] {
+  margin: 0.5em;
+}
+</style>
+
+<render-example></render-example>
+```html
+<j-input-decorator>
+  <span slot="prefix">Prefix</span>
+  <input type="text">
+  <button slot="suffix">Suffix</button>
+</j-input-decorator>
+```
--- src/components/InputDecorator.js
+++ src/components/InputDecorator.js
@@ -1,6 +1,93 @@
-import {DefineElementMixin} from '../util/DefineElementMixin.js';
+import { DefineElementMixin } from '../util/DefineElementMixin.js';
+
+const styles = `
+/*
+ Alternative way of positioning the prefix and suffix elements
+ No need to set the padding on the input element, or the height of the slots, but makes the host
+ "vulnerable" to global styling.
+*/
+/*
+  :host {
+    display: grid;
+    width: max-content;
+  }
+
+  slot[name],
+  ::slotted(input) {
+    grid-column: 1;
+    grid-row: 1;
+  }
+
+  slot[name] {
+    display: block;
+    z-index: 1;
+    align-self: center;
+    width: max-content;
+  }
+
+  slot[name=suffix] {
+    margin-inline-start: auto;
+  }
+*/
+
+  :host {
+    display: contents;
+  }
+
+  slot[name] {
+    display: inline-flex;
+    align-items: center;
+    position: absolute;
+    pointer-events: none;
+  }
+
+  slot[name]::slotted(:is(button, a, select, input)) {
+    pointer-events: auto;
+  }
+`;
 
 export class InputDecorator extends DefineElementMixin(HTMLElement) {
+  connectedCallback() {
+    if (!this.shadowRoot) {
+      this.attachShadow({mode: 'open'});
+      this.shadowRoot.innerHTML = `
+        <style>${styles}</style>
+        <slot name="prefix"></slot>
+        <slot></slot>
+        <slot name="suffix"></slot>
+      `;
+    }
+    if (!this.__mutationObserver) {
+      this.__mutationObserver = new MutationObserver(this._onMutation.bind(this));
+    }
+
+    this.__mutationObserver.observe(this, { childList: true });
+
+    this._onMutation();
+  }
+
+  disconnectedCallback() {
+    this.__mutationObserver.disconnect();
+  }
+
+  _onMutation() {
+    const prefix = this.shadowRoot.querySelector('slot[name="prefix"]');
+    const suffix = this.shadowRoot.querySelector('slot[name="suffix"]');
+    const input = this.querySelector('input');
+    // input.style.paddingInlineStart = input.style.paddingInlineEnd = '';
+
+    const prefixRect = prefix.getBoundingClientRect();
+    const suffixRect = suffix.getBoundingClientRect();
+    const inputRect = input.getBoundingClientRect();
+    // const inputComputedStyle = window.getComputedStyle(input);
+    // const inputPaddingInlineStart = parseInt(inputComputedStyle.getPropertyValue('padding-inline-start'));
+    // const inputPaddingInlineEnd = parseInt(inputComputedStyle.getPropertyValue('padding-inline-end'));
+    input.style.paddingInlineStart = prefixRect.width + 'px';
+    input.style.paddingInlineEnd = suffixRect.width + 'px';
+
+    prefix.style.height = suffix.style.height = inputRect.height + 'px';
+    suffix.style.marginInlineStart = -suffixRect.width + 'px';
+  }
 }
 
 InputDecorator.defineElement();
