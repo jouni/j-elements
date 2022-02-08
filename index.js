81022c48f718cc0f665a5a8958fd9adf96af9be5
Jouni Koivuviita <jouni@vaadin.com>
2022-02-08 21:50:16 +0200

    cleanup

---
+++ index.js
@@ -0,0 +1,20 @@
+// Components (custom elements)
+import { Field } from './src/components/Field.js';
+import { FieldGroup } from './src/components/FieldGroup.js';
+import { InputDecorator } from './src/components/InputDecorator.js';
+
+// Utilities
+import { LightStyleMixin } from './src/util/LightStyleMixin.js';
+import { MutationAnimationMixin } from './src/util/MutationAnimationMixin.js';
+import { Portal } from './src/util/Portal.js';
+import { Stylable } from './src/util/Stylable.js';
+
+export {
+  Field,
+  FieldGroup
+  InputDecorator,
+  LightStyleMixin,
+  MutationAnimationMixin,
+  Portal,
+  Stylable
+};
